218bf1f468d435587ec4dd379759785b649f029f
eyal <narnavg@gmail.com>
2022-06-30 09:23:19 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+function test() {
+  console.log("in");
+  io.emit("chat message", 10);
+}
+// setInterval(test, 2000);
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
